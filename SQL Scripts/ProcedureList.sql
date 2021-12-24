2c4d4634185c312717256536dfc59048536c2a3d
KayDee <phankhaidong0712@gmail.com>
2021-12-24 14:42:39 +0700

    fix conversion-dl, lostupdate, add procedure

--- SQL Scripts/ProcedureList.sql
+++ SQL Scripts/ProcedureList.sql
@@ -61,3 +61,46 @@ DECLARE @MATCH INT
 EXEC @MATCH = sp_KiemTraKVHD @KVHD, @DCGH
 PRINT @MATCH
 GO
+
+--Ràng buộc R1: Số chi nhánh của bảng đối tác bằng 
+--số chi nhánh của đối tác đó trong bảng chi nhánh. 
+--(thêm, xóa chi nhánh của đối tác → số chi nhánh)
+
+CREATE OR ALTER 
+TRIGGER TG_CHINHANH_INSERT
+ON CHINHANH FOR INSERT
+AS
+BEGIN
+	UPDATE HOPDONG
+	SET SOCHINHANH = SOCHINHANH + 1
+	WHERE MAHD IN (SELECT DISTINCT inserted.MAHD FROM inserted)
+END
+
+
+GO
+CREATE OR ALTER 
+TRIGGER TG_CHINHANH_DELETE
+ON CHINHANH FOR INSERT
+AS
+BEGIN
+	UPDATE HOPDONG
+	SET SOCHINHANH = SOCHINHANH - 1
+	WHERE MAHD IN (SELECT DISTINCT deleted.MAHD FROM deleted)
+END
+
+
+
+--Ràng buộc R2: Doanh thu của đối tác được tính bằng tổng số tiền sản phẩm của đối tác bán ra trong vòng 1 tháng 
+--(Tổng số tiền sản phẩm được tính bằng số tiền của tất cả các hóa đơn của đối tác đó). 
+--Sau 1 tháng doanh thu sẽ được khởi tạo lại.
+GO
+CREATE PROC PROC_TIM_DOANH_THU_DOITAC
+AS
+BEGIN
+	UPDATE DOITAC
+	SET DOANHTHU = (SELECT SUM(DH.TONGTIEN)
+					FROM DONHANG DH
+					WHERE MONTH(DH.THOIGIANLAP) = MONTH(GETDATE())
+					AND YEAR(DH.THOIGIANLAP) = YEAR(GETDATE())
+					AND DH.MADT = DOITAC.MADT)
+END
